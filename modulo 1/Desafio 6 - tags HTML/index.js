7af2974df63cd1337d8ab7f3de025256789450bf
fabio <fabiofelipe_sh@hotmail.com>
2024-10-03 23:03:45 -0300

    Initial commit

---
+++ modulo 1/Desafio 6 - tags HTML/index.js
@@ -0,0 +1,7 @@
+function mouseOver(){
+    document.getElementById("paragrafo").innerHTML = "usa-se a tag p";
+}
+// 
+function mouseOut(){
+    document.getElementById("paragrafo").innerHTML = "Parágrafo";
+ }
